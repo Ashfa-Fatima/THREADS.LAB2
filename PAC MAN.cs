deeb061e17533a1aed40d26b36a2c9494aa3398f
Ashfa-Fatima <ashyjenny45@gmail.com>
2025-09-24 10:33:09 +0500

    Add files via upload

---
+++ PAC MAN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static void Main()
+    {
+        string line = "...................................................................................................";
+        int length = line.Length;
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write("\r" + new string(' ',i) + "Sehar" + line.Substring(i + 1));
+            Thread.Sleep(200);
+            Console.Write("\r" + new string(' ', i + 1) + line.Substring(i + 1));
+            Thread.Sleep(250);
+
+        }
+        Console.Write("\r" + new string(' ', length) + "Sehar");
+        Console.WriteLine("\n\n Game Over Dear! PAC MAN ate all the dots like sehar ate Ants!");
+        Console.ReadLine();
+    }
+}
