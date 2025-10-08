c272f1b9523aa19fc406abac620c8c3f059faf5d
Ashfa-Fatima <ashyjenny45@gmail.com>
2025-10-08 09:10:35 +0500

    Add files via upload
    THREAD LIFE CYCLE PROGRAM LAB4

---
+++ LIFECYCLETHREAD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+class Program
+
+{
+    static void Main()
+    {
+
+
+        Thread life1 = new Thread(Life1);
+        Console.WriteLine("State:" + life1.ThreadState);
+
+        life1.Start();
+        Console.WriteLine("State:" + life1.ThreadState);
+
+      
+
+        Thread.Sleep(1000);
+        Console.WriteLine("State:" + life1.ThreadState);
+
+        life1.Abort();
+        Console.WriteLine("State:" + life1.ThreadState);
+
+        life1.Start();
+        Console.WriteLine("State:" + life1.ThreadState);
+
+        life1.Join();
+        Console.WriteLine("State:" + life1.ThreadState);
+
+
+    }
+    static void Life1()
+    {
+        Thread.Sleep(5000);
+
+    }
+}
