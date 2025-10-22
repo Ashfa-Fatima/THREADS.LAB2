97eaedd700e96fc3a699642f2ac1c357047e4fe9
Ashfa-Fatima <ashyjenny45@gmail.com>
2025-10-22 09:07:37 +0500

    Add files via upload

---
+++ task1_Lab6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class UDPClientLossDemo
+{
+    static void Main()
+    {
+        UdpClient client = new UdpClient();
+        client.Connect("192.168.18.39", 9000);
+
+        for (int i = 1; i <= 50; i++)
+        {
+            string msg = $"Packet{i}";
+            byte[] data = Encoding.ASCII.GetBytes(msg);
+            client.Send(data, data.Length);
+        }
+        Console.WriteLine("All packet sent!");
+        client.Close();
+        }
+    }
