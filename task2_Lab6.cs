7bcb755cd12d72269d4e5b1730730cadc197c6c2
Ashfa-Fatima <ashyjenny45@gmail.com>
2025-10-22 09:35:57 +0500

    Add files via upload

---
+++ task2_Lab6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class UDPClient
+
+{
+    static void Main()
+    {
+        UdpClient client = new UdpClient();
+        client.Connect("192.168.18.39", 9000);
+
+        Console.WriteLine("Enter your integer value(client side");
+        int clientValue = int.Parse(Console.ReadLine());
+
+        byte[]data =
+        Encoding.ASCII.GetBytes(clientValue.ToString());
+        client.Send(data,data.Length);
+
+        IPEndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
+
+        byte[] receiveData = client.Receive(ref serverEP);
+        string result = Encoding.ASCII.GetString(receiveData);
+
+        Console.WriteLine($" Sum recived from server:{result}");
+        client.Close();
+    }
+}
